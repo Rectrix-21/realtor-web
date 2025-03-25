dcb8f8c3e333ec749660507e107b1ea4e2b062f3
Rectrix-21 <abhijithkrishnan14@gmail.com>
2025-03-24 18:46:10 -0600

    added authentication front end

---
+++ app/authentication-page/layout.js
@@ -0,0 +1,10 @@
+import "../globals.css";
+
+export const metadata = {
+  title: "Sign Up",
+  description: "Sign up for an account",
+};
+
+export default function RootLayout({ children }) {
+  return <div>{children}</div>;
+}

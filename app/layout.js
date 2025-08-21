29ff751ff55bb934c1d2e60b6a53a7ea61b1f4b0
Rectrix-21 <abhijithkrishnan14@gmail.com>
2025-08-20 20:54:42 -0600

    optimized build

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,13 @@ export const metadata = {
   description: "Realtor website for Havenly",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
